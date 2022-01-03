f73272ae34a7d11202b6794a9064365f49d67aba
Rohan Gupta <guptarohan2002@gmail.com>
2022-01-03 20:40:08 +0530

    check push

--- routes/product.js
+++ routes/product.js
@@ -6,6 +6,7 @@ const router = require('express').Router();
 
 
 router.post('/', verifyTokenAndAdmin, async(req, res) => {
+    console.log(req.body);
     const newProduct = new Product(req.body);
     try {
         const savedProduct = await newProduct.save();
